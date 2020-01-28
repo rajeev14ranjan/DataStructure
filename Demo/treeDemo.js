6ac159f53beccdc13b9c4f9d689d62c2fbf6afa0
Rajeev Ranjan <rajeevranjan@MacMini.local>
2020-01-29 00:57:00 +0530

    refactor tree

--- Demo/treeDemo.js
+++ Demo/treeDemo.js
@@ -1,4 +1,5 @@
 const BST = require('../src/Tree');
+console.clear();
 
 let tree = new BST(50);
 tree.insert(30);
@@ -12,27 +13,26 @@ tree.insert(35);
 tree.insert(55);
 
 
-// console.log(tree);
-
-// console.log('-----------');
-
  console.log(`Depth of Tree = ${tree.depth()}`);
-
-let posta =[], prea =[], ina = [], ddf = [];
+ tree.show();
+let postOrder =[], preOrder =[], inOrder = [], ddf = [];
 function addToIterator(stor, n){ stor.push(n)};
 
-tree.DFS(addToIterator.bind(null,prea),'pre');
-tree.DFS(addToIterator.bind(null,ina),'in');
-tree.DFS(addToIterator.bind(null,posta),'post');
+tree.DFS(addToIterator.bind(null,preOrder),'pre');
+tree.DFS(addToIterator.bind(null,inOrder),'in');
+tree.DFS(addToIterator.bind(null,postOrder),'post');
 tree.BFS(addToIterator.bind(null,ddf));
 
 
-console.log('PRE order traversal\n ' + prea.join(' -> '));
-console.log('IN order traversal\n ' +ina.join(' -> '));
-console.log('POST order traversal\n ' +posta.join(' -> '));
+console.log('PRE order traversal\n ' + preOrder.join(' -> '));
+console.log('IN order traversal\n ' +inOrder.join(' -> '));
+console.log('POST order traversal\n ' +postOrder.join(' -> '));
 console.log('BFS\n ' +ddf.join(' -> '));
+console.log();
+
 tree.LevelOrder();
 tree.ZigZagOrder();
+
 
 
 
